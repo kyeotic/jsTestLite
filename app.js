2be3b64195a3e09ad08d9ab5f5a376af98edf7a6
Timothy Moran <tyrsius@gmail.com>
2014-01-06 20:18:40 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+(function() {
+	var testHost = $('#testHost');
+		
+	var rerunTests = function() {
+	
+		var testFrame = $('<iframe id="testFrame" src="runner.html"></iframe>');
+		
+		//Reset the test frame
+		testHost.empty().append(testFrame);
+		
+		var testFrameDoc = testFrame.contents()[0],
+			testFrameBody = testFrame.contents().find('body');
+		
+		var code = $('#userTests').val(),
+			codeScript = testFrameDoc.createElement('script'),
+			tests = $('#userTests').val(),
+			testsScript = testFrameDoc.createElement('script');
+		
+		codeScript.type = 'text/javascript';
+		codeScript.text = code;
+		testFrameBody.appendChild(codeScript);
+		
+		codeScript.type = 'text/javascript';
+		codeScript.text = code;
+		testFrameBody.appendChild(testsScript);
+		
+		var runnerScript = testFrameDoc.createElement('script');
+		runnerScript.type = 'text/javascript';
+		runnerScript.src = 'runner.js';
+		testFrameBody.appendChild(runnerScript);
+	};
+	
+	$('textarea').focus(function() {
+		$(this).keyup(function() {
+			rerunTests();
+		});
+	});
+})();
