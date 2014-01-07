105392189d0a5b88e787817ae25d370699164baa
Timothy Moran <tyrsius@gmail.com>
2014-01-07 06:08:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,19 +8,13 @@
     //Reset the test frame
     testHost.empty().append(testFrame);
     
-    var testFrameDoc = testFrame.contents()[0],
-      testFrameBody = testFrame.contents().find('body')[0];
-    
     var code = $('#userTests').val(),
-      codeScript = testFrameDoc.createElement('script'),
-      tests = $('#userTests').val(),
-      testsScript = testFrameDoc.createElement('script');
+      tests = $('#userTests').val(),;
 
     var frame = window.frames[0];
-
-    frame.window.eval(code);
-    frame.window.eval(codetestsScript);
-    frame.window.eval(codeScript);
+    
+	frame.__codeScript = code;
+	frame.__testScript = tests;
   };
   
   $('textarea').keyup(function() {
