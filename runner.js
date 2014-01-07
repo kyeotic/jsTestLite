d41ee504c7d7ad37d94c5bdf530076456c930f74
Timothy Moran <tyrsius@gmail.com>
2014-01-07 00:04:02 -0800

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,13 +1,34 @@
 (function() {
 	var jasmineEnv = jasmine.getEnv();
 	jasmineEnv.updateInterval = 250;
-	
+
 	var htmlReporter = new jasmine.HtmlReporter();
 	jasmineEnv.addReporter(htmlReporter);
-	
+
 	jasmineEnv.specFilter = function(spec) {
 		return htmlReporter.specFilter(spec);
 	};
-	
-	jasmineEnv.execute();
+
+	function execJasmine() {
+		jasmineEnv.execute();
+	};
+
+	window.onload = function() {
+		debugger;
+
+		var code = window.__codeScript,
+		  codeScript = document.createElement('script'),
+		  tests = window.__testScript,
+		  testsScript = document.createElement('script');
+
+		codeScript.type = 'text/javascript';
+		codeScript.text = code;
+		testFrameBody.appendChild(codeScript);
+		
+		codeScript.type = 'text/javascript';
+		codeScript.text = code;
+		testFrameBody.appendChild(testsScript);
+
+		execJasmine();
+	};
 })();
