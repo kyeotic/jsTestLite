e97ef093b36bb053c04aa95e8d3213d733c187ce
Timothy Moran <tyrsius@gmail.com>
2014-01-07 12:46:55 -0800

    adding cookie storage

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-(function() {
+var app = app || {};
+
+(function($) {
 	var defaults = {
 		codeSmall: 13,
 		codeMedium: 21,
@@ -6,11 +8,22 @@
 		testSmall: 160,
 		testMedium: 320,
 		testLarge: 550,
-		testDebounce: 500
+		testDebounce: 500,
+		cookieName: 'Default'
 	};
 
 	//Init Tab-Override
 	$('textarea').tabOverride();
+
+	var resetFields = function() {
+		$('#userCode').val('');
+		$('#userTests').val("describe('', function() {\n\tit('', function() {\n\t\t\n\t});\n});");
+	};
+
+	/* 
+		Setup Field Size and Size Control
+		======================================================
+	*/
 
 	var setTextAreaRows = function(size) {
 		$('textarea').attr('rows', size);
@@ -21,7 +34,107 @@
 		testFrameHeight = size;
 		$('iframe').height(size);
 	};
+
+	$('#codeSmall').click(function() { setTextAreaRows(defaults.codeSmall); });
+	$('#codeMedium').click(function() { setTextAreaRows(defaults.codeMedium); });
+	$('#codeLarge').click(function() { setTextAreaRows(defaults.codeLarge); });
 	
+	$('#testSmall').click(function() { setTestFrameHeight(defaults.testSmall); });
+	$('#testMedium').click(function() { setTestFrameHeight(defaults.testMedium); });
+	$('#testLarge').click(function() { setTestFrameHeight(defaults.testLarge); });
+	
+	/*
+		Setup Example Button
+		======================================================
+	*/
+	$('#codeExample').click(function() {
+	    var examples = $('#examples').contents().filter(function() { return this.nodeType === 8; }),
+	        codeExample = examples.get(0).nodeValue.trim(),
+	        testExample = examples.get(1).nodeValue.trim();
+	    $('#userCode').val(codeExample);
+	    $('#userTests').val(testExample);
+	    rerunTests();
+	});
+
+	/*
+		Setup Cookie Manager
+		======================================================
+	*/
+
+	//Bind the toggle button
+	$('#toggleCookies').click(function() { $('#cookieContainer').slideToggle(); } );
+
+	//Retriece the cookie list, and selected cookie
+	var cookieList = app.cookie.list(app.activeCookieName),
+		activeCookie = app.cookie.get(app.activeCookieName);
+
+	//Select the first cookie if none is selected
+	if (!activeCookie && cookieList.length > 0) {
+		activeCookie = cookieList[0];
+	}
+
+	//Load the cookie if present
+	if (activeCookie) {
+		loadContentFromCookie();
+	//Otherwise, make a default cookie
+	} else {
+		$('#cookieContainer').hide();
+		activeCookie = defaults.cookieName;
+		cookieList.push(activeCookie);
+		resetFields();
+	}
+
+	//Populate the dropdown
+	if (cookieList.length > 0) {
+		$.each(cookieList, function (i, item) {
+		    $('#cookieList').append($('<option>', { value: item, text : item }));
+		});
+	}
+
+	//Save should set the active cookie, save the content, add a dropdown option, and select it
+	$('#cookieSave').click(function() {
+		if ($('#cookieList').find('[value=' + activeCookie + ']').length > 0) {
+			alert("A cookie by this name already exists");
+			return;
+		}
+		activeCookie = this.value;
+		saveContentToCookie();
+
+		$('#cookieList').append($('<option>', { value: cookieName, text : cookieName }));
+		$('#.cookieList').val(activeCookie);
+	});
+
+	//Delete should remove the current cookie, remove the dropdown option, and clear the fields
+	$('#cookieDelete').click(function() {
+		app.cookie.remove(activeCookie);
+		$('#cookieList').find('[value=' + activeCookie + ']').remove();
+		resetFields();
+	});	
+
+	//Change should load the selected cookie
+	$('#cookieList').change(function() {
+		var cookieName = this.value;
+		if (activeCookie === cookieName)
+			return;
+		activeCookie = cookieName;
+		loadContentFromCookie();
+	});
+
+	var saveContentToCookie = function() {
+		app.cookie.set(activeCookie, { code: $('#userCode').val(), tests: $('#userTests').val()}, 1000);
+	};
+	
+	var loadContentFromCookie = function() {
+		var cookie = app.cookie.get(activeCookie);
+		$('#userCode').val(cookie.code);
+		$('#userTests').val(cookie.tests);
+	};
+
+	/*
+		Setup Test Runner
+		======================================================
+	*/
+
 	var testHost = $('#testHost');
 	var rerunTests = function() {
 		var testFrame = $('<iframe id="testFrame" src="runner.html"></iframe>');
@@ -32,33 +145,19 @@
 
 		window.frames[0].__codeScript = $('#userCode').val();
 		window.frames[0].__testScript = $('#userTests').val();
-	};
-	
-	$('textarea').keyup($.debounce(defaults.testDebounce, rerunTests ));
-	
-	$('#codeSmall').click(function() { setTextAreaRows(defaults.codeSmall); });
-	$('#codeMedium').click(function() { setTextAreaRows(defaults.codeMedium); });
-	$('#codeLarge').click(function() { setTextAreaRows(defaults.codeLarge); });
-	
-	$('#testSmall').click(function() { setTestFrameHeight(defaults.testSmall); });
-	$('#testMedium').click(function() { setTestFrameHeight(defaults.testMedium); });
-	$('#testLarge').click(function() { setTestFrameHeight(defaults.testLarge); });
 
-	$('#codeExample').click(function() {
-	    var examples = $('#examples').contents().filter(function() { return this.nodeType === 8; }),
-	        codeExample = examples.get(0).nodeValue.trim(),
-	        testExample = examples.get(1).nodeValue.trim();
-	    $('#userCode').val(codeExample);
-	    $('#userTests').val(testExample);
-	    rerunTests();
-	});
-	
-	var resetFields = function() {
-		$('#userCode').val();
-		$('#userTests').val("describe('', function() {\n\tit('', function() {\n\t\t\n\t});\n});");
+		saveContentToCookie();
 	};
-	resetFields(); //Call it to start
+
+	$('textarea').keyup($.debounce(defaults.testDebounce, rerunTests ));
+
+	/*
+		Setup Reset Button
+		======================================================
+	*/
+	
+	
 	
 	$('#codeClear').click(function() { resetFields(); rerunTests(); });
 	
-})();
+})(jQuery);
---
+++ cookie.js
@@ -0,0 +1,56 @@
+var app = app || {};
+app.activeCookieName = '__activeCookie';
+app.cookie = (function(){
+      var set = function (name, value, days) {
+      	var expires;
+        if (days) {
+          var date = new Date();
+          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+          expires = "; expires=" + date.toGMTString();
+        }
+        else {
+        	expires = "";
+        }
+        document.cookie = name + "=" + JSON.stringify(value) + expires + "; path=/";
+      };
+
+      var get = function (name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+          }
+          if (c.indexOf(nameEQ) == 0) {
+            return JSON.parse(c.substring(nameEQ.length, c.length));
+          }
+        }
+        return null;
+      };
+
+      var remove = function (name) {
+        set(name, "", -1);
+      };
+
+      var list = function (exclude) {
+      	var result = [],
+					cookies = document.cookie.split(';'),
+          exclude = exclude || [];
+        if (!(exclude instanceof Array))
+          exclude = [exclude];
+				for (var i = 0 ; i < cookies.length; i++) {
+          var cookie = cookies[i].split('=')[0];
+          if (exclude.indexOf(cookie) !== -1)
+					 result.push(cookie);
+				}						
+				return result;
+      };
+      
+      return {
+        set: set,
+        get: get,
+        remove: remove,
+        list: list
+      };
+})();
